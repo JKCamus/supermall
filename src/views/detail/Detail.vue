6835808061e370acb1c21d5e23637c51b4bca361
Max <JKcamus@163.com>
2020-02-28 08:46:31 +0800

    paraminfo completed

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -4,7 +4,8 @@
     <detail-swiper :top-images="topImages"></detail-swiper>
     <detail-base-info :goods="goods"></detail-base-info>
     <detail-shop-info :shop="shop"></detail-shop-info>
-    <!-- <detail-goods-info :goods="goods"></detail-goods-info> -->
+    <detail-goods-info :goods="goods"></detail-goods-info>
+    <detail-param-info :param-info="paramInfo"></detail-param-info>
   </div>
 </template>
 <script>
@@ -12,11 +13,13 @@
   import DetailSwiper from 'views/detail/childComps/DetailSwiper'
   import DetailBaseInfo from 'views/detail/childComps/DetailBaseInfo'
   import DetailShopInfo from 'views/detail/childComps/DetailShopInfo'
-
+  import DetailGoodsInfo from 'views/detail/childComps/DetailGoodsInfo'
+  import DetailParamInfo from 'views/detail/childComps/DetailParamInfo'
   import {
     getDetail,
     Goods,
-    Shop
+    Shop,
+    GoodsParam
   } from 'network/detail'
   export default {
     name: "Detail",
@@ -24,14 +27,18 @@
       DetailNavBar,
       DetailSwiper,
       DetailBaseInfo,
-      DetailShopInfo
+      DetailShopInfo,
+      DetailGoodsInfo,
+      DetailParamInfo
     },
     data() {
       return {
         iid: null,
         topImages: [],
         goods: {},
-        shop:{}
+        shop: {},
+        paramInfo: {},
+        detailInfo: {}
       }
     },
     created() {
@@ -41,19 +48,23 @@
       //   根据iid请求详情页数据
       getDetail(this.iid).then(res => {
         const data = res.result
-        console.log(res);
+        // console.log(res);
         //获取详情页顶部轮播图
         this.topImages = data.itemInfo.topImages
         //根据iid请求获取商品信息
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
         // console.log(this.goods);
         // 获取店铺信息
-        this.shop=new Shop(data.shopInfo)
+        this.shop = new Shop(data.shopInfo)
         // console.log(this.shop);
-        
+        // 获取详细信息
+        this.detailInfo = data.detailInfo
+        // console.log(this.detailInfo);
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+        // console.log(this.paramInfo);
+
       })
     },
-
   }
 
 </script>
---
+++ src/views/detail/childComps/DetailGoodsInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods-info">
+    1212
+  </div>
+</template>
+<script>
+  export default {
+    name: "DetailGoodsInfo"
+  }
+
+</script>
+<style scoped>
+</style>
